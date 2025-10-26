34cff02cee4469204fbe4ff0aa2ac2300786fb9b
Prasanna Kondapaneni <prasanna@carbonmint.com>
2025-10-26 08:17:45 +0530

    shell scripts for remote deployment

--- terraform/recreate-ec2.sh
+++ terraform/recreate-ec2.sh
@@ -4,7 +4,7 @@
 # This script safely destroys and recreates EC2 instances while preserving other infrastructure
 
 set -e
-set -a; source .env; set +a;
+set -a; source ../.env.local; set +a;
 # Check that TF_TF_VAR_github_token and TF_VAR_github_repository_url
 # are set
 if [ -z "$TF_VAR_github_token" ] || [ -z "$TF_VAR_github_repository_url" ]; then
